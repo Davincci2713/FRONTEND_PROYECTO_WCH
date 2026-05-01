96802775668cc3ca484563fff660cc35293f19de
Davincci2713 <110733398+Davincci2713@users.noreply.github.com>
2026-05-01 17:17:49 -0500

    Login con nueva interfaz agregado

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -8,12 +8,12 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
 
-import 'package:frontend_proyecto/main.dart';
+import 'package:frontend_proyecto/login.dart';
 
 void main() {
   testWidgets('Counter increments smoke test', (WidgetTester tester) async {
     // Build our app and trigger a frame.
-    await tester.pumpWidget(const MyApp());
+    await tester.pumpWidget(const Mundial2026App());
 
     // Verify that our counter starts at 0.
     expect(find.text('0'), findsOneWidget);
