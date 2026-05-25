87913a29a73a0f9b14453b3a3c77fe944e8ad13e
Nicolas Barbosa <niko@niko-macbook.local>
2026-05-24 21:52:59 -0500

    Fix: use @JS external function instead of extension type for js_interop
    extension type requires Dart >=3.3.0 but pubspec specifies >=3.2.0.
    Plain @JS('window.location.reload') works from Dart >=3.1.0.

--- lib/utils/platform_reload_web.dart
+++ lib/utils/platform_reload_web.dart
@@ -1,18 +1,10 @@
 import 'dart:js_interop';
 import 'package:flutter/foundation.dart';
 
-extension type _Location._(JSObject _) implements JSObject {
-  external void reload();
-}
-
-extension type _Window._(JSObject _) implements JSObject {
-  external _Location get location;
-}
-
-@JS('window')
-external _Window get _jsWindow;
+@JS('window.location.reload')
+external void _windowLocationReload();
 
 void reloadPlatform(VoidCallback fallbackReload) {
   fallbackReload();
-  _jsWindow.location.reload();
+  _windowLocationReload();
 }
